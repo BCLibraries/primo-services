bf5e147934beb2b6ab7c1660324807bce0b9d85a
Ben Florin <benajmin.florin@bc.edu>
2012-01-30 12:11:18 -0500

    Removed SplAutoloader requirement

--- XServices.php
+++ XServices.php
@@ -1,6 +1,5 @@
 <?php
 
 require_once 'HTTP/Request2.php';
-require_once 'SplClassLoader/SplClassLoader.php';
 require_once("bclib/autoload.php");
 $autoloader = new \edu\bc\library\Autoload(__DIR__ . \DIRECTORY_SEPARATOR);
