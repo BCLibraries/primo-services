5b0011f063c3bd93496539fd4dfee7bbfbffa41e
Ben Florin <benjamin.florin@gmail.com>
2012-06-18 15:56:24 -0400

    Consolidating all autoloading to SplClassLoader

--- XServices.php
+++ XServices.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once 'HTTP/Request2.php';
-require_once("bclib/autoload.php");
-$autoloader = new \edu\bc\library\Autoload(__DIR__ . \DIRECTORY_SEPARATOR);
+require 'SplClassLoader/SplClassLoader.php';
+$dm_loader = new SplClassLoader('BCLib\XServices', __DIR__);
+$dm_loader->register();
