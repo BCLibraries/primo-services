2c2c61f0eda961ae7330f749b75379d27503663a
Ben Florin <benjamin.florin@bc.edu>
2013-04-09 16:50:46 -0400

    Added basic query terms

---
+++ src/BCLib/PrimoServices/QueryTerm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BCLib\PrimoServices;
+
+class QueryTerm
+{
+    private $_index;
+    private $_precision;
+    private $_term;
+
+    const CONTAINS = 'contains';
+    const EXACT = 'exact';
+    const BEGINS = 'begins_with';
+
+    public function keyword($keyword, $precision = QueryTerm::CONTAINS)
+    {
+        $this->set('any', $precision, $keyword);
+    }
+
+    public function title($title, $precision = QueryTerm::CONTAINS)
+    {
+        $this->set('title', $precision, $title);
+    }
+
+    public function creator($creator, $precision = QueryTerm::CONTAINS)
+    {
+        $this->set('creator', $precision, $creator);
+    }
+
+    public function queryString()
+    {
+        return $this->_index . ',' . $this->_precision . ',' . $this->_term;
+    }
+
+    public function set($index, $precision, $term)
+    {
+        if ($precision != QueryTerm::CONTAINS)
+        {
+            throw new \Exception($precision . ' is not a valid query relation');
+        }
+
+        $this->_index = $index;
+        $this->_precision = $precision;
+        $this->_term = $term;
+    }
+}
---
+++ test/BCLib/PrimoServices/QueryTermTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: florinb
+ * Date: 4/9/13
+ * Time: 4:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace BCLib\PrimoServices;
+
+
+class QueryTermTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var QueryTerm */
+    private $_query_term;
+
+    public function setUp()
+    {
+        $this->_query_term = new QueryTerm();
+    }
+
+    public function testAddingGenericQueryTermSucceeds()
+    {
+        $expected = 'title,contains,Otters';
+        $this->_query_term->set('title', 'contains', 'Otters');
+        $this->assertEquals($expected, $this->_query_term->queryString());
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testBadPrecisionThrowsException()
+    {
+        $this->_query_term->set('title','not-a-precision','Otters');
+    }
+
+    public function testAddingKeywordSucceeds()
+    {
+        $expected = 'any,contains,Otters';
+        $this->_query_term->keyword('Otters');
+        $this->assertEquals($expected, $this->_query_term->queryString());
+    }
+
+    public function testAddingTitleSucceeds()
+    {
+        $expected = 'title,contains,Otters';
+        $this->_query_term->title('Otters');
+        $this->assertEquals($expected, $this->_query_term->queryString());
+    }
+
+    public function testAddingCreatorSucceeds()
+    {
+        $expected = 'creator,contains,Aesop';
+        $this->_query_term->creator('Aesop');
+        $this->assertEquals($expected, $this->_query_term->queryString());
+    }
+}
