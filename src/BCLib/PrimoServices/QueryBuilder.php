6eeae74408e332ce3de3828f5cd6f4952ba3837c
Ben Florin <benjamin.florin@bc.edu>
2014-01-31 11:54:51 -0500

    Added QueryBuilder to isolate constrctor calls

---
+++ src/BCLib/PrimoServices/QueryBuilder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace BCLib\PrimoServices;
+
+class QueryBuilder
+{
+    /**
+     * @var \BCLib\PrimoServices\Query
+     */
+    protected $_query;
+
+    protected $_institution;
+
+    public function __construct($institution)
+    {
+        $this->_institution = $institution;
+        $this->_buildQuery();
+    }
+
+    public function getQuery()
+    {
+        $finished_query = $this->_query;
+        $this->_buildQuery();
+        return $finished_query;
+    }
+
+    public function keyword($keyword, $precision = QueryTerm::CONTAINS)
+    {
+        return $this->_addTerm('keyword', $keyword, $precision);
+    }
+
+    public function title($title, $precision = QueryTerm::CONTAINS)
+    {
+        return $this->_addTerm('title', $title, $precision);
+    }
+
+    public function creator($creator, $precision = QueryTerm::CONTAINS)
+    {
+        return $this->_addTerm('creator', $creator, $precision);
+    }
+
+    public function subject($subject, $precision = QueryTerm::CONTAINS)
+    {
+        return $this->_addTerm('subject', $subject, $precision);
+    }
+
+    public function date($date)
+    {
+        return $this->_addTerm('date', $date, $precision);
+    }
+
+    public function isbn($isbn)
+    {
+        return $this->_addTerm('isbn', $isbn, $precision);
+    }
+
+    public function issn($issn)
+    {
+        return $this->_addTerm('issn', $issn, $precision);
+    }
+
+    public function custom($index, $precision, $term)
+    {
+        $term = new QueryTerm();
+        $term->set($index, $precision, $term);
+        $this->_query->addTerm($term);
+        return $this;
+    }
+
+    protected function _addTerm($name, $value, $precision)
+    {
+        $term = new QueryTerm();
+        $term->$name($value, $precision);
+        $this->_query->addTerm($term);
+        return $this;
+    }
+
+    protected function _buildQuery()
+    {
+        $this->_query = new Query($this->_institution);
+    }
+} 
